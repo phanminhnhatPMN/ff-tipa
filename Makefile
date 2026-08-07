1df75b8da3307b9595d2a33da3cc4315aeeacdee
Nhat Phan <phanminhnhat205@gmail.com>
2026-08-08 03:10:03 +0700

    Add SpringBoard & BackBoardServices system overlay entitlements and UIWindow setup

--- Makefile
+++ Makefile
@@ -14,6 +14,7 @@ PMNDev_CFLAGS += -fobjc-arc \
 -Wno-unused-variable
 
 PMNDev_FRAMEWORKS += CoreGraphics QuartzCore UIKit Foundation
+PMNDev_PRIVATE_FRAMEWORKS += BackBoardServices GraphicsServices IOKit SpringBoardServices
 
 PMNDev_CODESIGN_FLAGS += -Sent.plist
 
--- main.mm
+++ main.mm
@@ -8,17 +8,19 @@
 @implementation PMNAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    CGRect frame = [UIScreen mainScreen].bounds;
-    self.window = [[UIWindow alloc] initWithFrame:frame];
-    self.window.windowLevel = UIWindowLevelStatusBar + 5000;
-    self.window.backgroundColor = [UIColor clearColor];
-    self.window.userInteractionEnabled = YES;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        CGRect frame = [UIScreen mainScreen].bounds;
+        self.window = [[UIWindow alloc] initWithFrame:frame];
+        self.window.windowLevel = UIWindowLevelStatusBar + 2000;
+        self.window.backgroundColor = [UIColor clearColor];
+        self.window.userInteractionEnabled = YES;
 
-    PMNDevOverlayView *overlay = [[PMNDevOverlayView alloc] initWithFrame:frame];
-    UIViewController *vc = [[UIViewController alloc] init];
-    vc.view = overlay;
-    self.window.rootViewController = vc;
-    self.window.hidden = NO;
+        PMNDevOverlayView *overlay = [[PMNDevOverlayView alloc] initWithFrame:frame];
+        UIViewController *vc = [[UIViewController alloc] init];
+        vc.view = overlay;
+        self.window.rootViewController = vc;
+        self.window.hidden = NO;
+    });
 
     return YES;
 }
