29f81b45d85d576a83d2ed31d60c0f014b62673b
Nhat Phan <phanminhnhat205@gmail.com>
2026-08-09 04:43:54 +0700

    Register overlay window context ID with SBSAccessibilityWindowHostingController in main.mm

--- main.mm
+++ main.mm
@@ -1,5 +1,10 @@
 #import <UIKit/UIKit.h>
+#import <objc/runtime.h>
 #import "PMNDevOverlay.h"
+
+@interface UIWindow (Private)
+- (unsigned int)_contextId;
+@end
 
 __attribute__((constructor))
 static void initializePMNDevHUDOverlay(void) {
@@ -7,7 +12,7 @@ static void initializePMNDevHUDOverlay(void) {
         static UIWindow *gPMNWindow = nil;
         CGRect frame = [UIScreen mainScreen].bounds;
         gPMNWindow = [[UIWindow alloc] initWithFrame:frame];
-        gPMNWindow.windowLevel = UIWindowLevelStatusBar + 2000;
+        gPMNWindow.windowLevel = 10000010.0;
         gPMNWindow.backgroundColor = [UIColor clearColor];
         gPMNWindow.userInteractionEnabled = YES;
 
@@ -16,8 +21,27 @@ static void initializePMNDevHUDOverlay(void) {
         vc.view = overlay;
         gPMNWindow.rootViewController = vc;
         gPMNWindow.hidden = NO;
+
+        Class hostingCls = objc_getClass("SBSAccessibilityWindowHostingController");
+        if (hostingCls) {
+            id hostingController = [[hostingCls alloc] init];
+            if ([gPMNWindow respondsToSelector:@selector(_contextId)]) {
+                unsigned int contextId = [gPMNWindow _contextId];
+                double level = 10000010.0;
+                NSMethodSignature *sig = [NSMethodSignature signatureWithObjCTypes:"v@:Id"];
+                if (sig) {
+                    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:sig];
+                    [inv setTarget:hostingController];
+                    [inv setSelector:NSSelectorFromString(@"registerWindowWithContextID:atLevel:")];
+                    [inv setArgument:&contextId atIndex:2];
+                    [inv setArgument:&level atIndex:3];
+                    [inv invoke];
+                }
+            }
+        }
     });
 }
+
 
 
 
